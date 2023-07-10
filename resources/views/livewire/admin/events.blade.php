b845ce5e7ceba7f83c46c95ae7d79f27cf11bd76
omarIbrahim2 <rady1753@gmail.com>
2023-07-11 00:50:16 +0300

    dashboard

--- resources/views/livewire/admin/events.blade.php
+++ resources/views/livewire/admin/events.blade.php
@@ -15,6 +15,10 @@
        <a href="{{route('create-events-view')}}" class="btn btn-success margin_res">Add Event</a>
     </div>
 
+    <form id="cancelEvent" action="{{route('cancel-event') }}" method="POST">
+        @csrf
+    </form>
+
        <table class="table table-bordered table-hover table-striped mb-4 admin_table">
     <thead>
         <tr>
@@ -44,11 +48,11 @@
              <td class="text-center">
 
                 <a href="{{route('update-events-view' , $event->id)}}" class="btn mb-2" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg></a>
-                <form action="{{route('cancel-event') }}" method="POST">
-                    @csrf
-                    <input type="hidden" name="id" value="{{ $event->id }}">
-                    <button type="submit" class="btn btn-danger" >cancel event</button>
-                </form>
+    
+                    
+                    <input form="cancelEvent" type="hidden" name="id" value="{{ $event->id }}">
+                    <button form="cancelEvent" type="submit" class="btn btn-danger" >cancel event</button>
+    
           </td>
         </tr>
          @endforeach
