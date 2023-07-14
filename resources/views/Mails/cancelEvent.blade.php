7c0a055b6372b41ccd9928003a2167eb2b6925dc
omarIbrahim2 <rady1753@gmail.com>
2023-07-14 05:59:35 +0300

    finfi

--- resources/views/Mails/cancelEvent.blade.php
+++ resources/views/Mails/cancelEvent.blade.php
@@ -1,4 +1,4 @@
-<p>Dear {{<!--user name-->}};</p>
-<p style="padding-start:8px">UnFortunatilly The <!--event name--> event is canceled</p>
+<p>Dear {{}}</p>
+<p style="padding-start:8px">UnFortunatilly The event is canceled</p>
 <p>Best Ragardes</p>
 <img src="{{asset('assets')}}/images/logo/logo.png}}" style="text-align:center">
