a90cba6a6863e564b0a9a82a98e4689000abcbb4
hussien10 <hussienmo385@gmail.com>
2023-07-23 20:56:10 +0300

    cart

--- resources/views/web/Card/Card.blade.php
+++ resources/views/web/Card/Card.blade.php
@@ -19,14 +19,20 @@
                     @csrf
                     <button type="submit" class="payment-button">
                         <img src="{{asset('assets')}}/images/payments/pay.png" alt="payment">
-                        <span><svg class="svg-inline--fa fa-chevron-right" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z"></path></svg><!-- <i class="fa-solid fa-chevron-right"></i> Font Awesome fontawesome.com --></span>
+                        <span><svg class="svg-inline--fa fa-chevron-right" aria-hidden="true" focusable="false"
+                                data-prefix="fas" data-icon="chevron-right" role="img"
+                                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg="">
+                                <path fill="currentColor"
+                                    d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z">
+                                </path>
+                            </svg><!-- <i class="fa-solid fa-chevron-right"></i> Font Awesome fontawesome.com --></span>
                     </button>
                 </form>
-                    <div class="total-price">
-                        <h3 >Total Price : {{ $card->totalPrice }} JOD</h3>
-                    </div>
+                <div class="total-price">
+                    <h3>Total Price : {{ $card->totalPrice }} JOD</h3>
+                </div>
 
-                    @foreach ($card->services as $service)
+                @foreach ($card->services as $service)
                 <div class="service-item">
                     <div class="row">
                         <div class="col-md-2 col-12">
@@ -50,12 +56,17 @@
                                 </div>
                                 <div class="service-booking">
                                     <label for="">Quantity</label>
-                                    <p name="quantity"  class="num_cart_item form-control mb-3">{{ $service->pivot->quantity }}</p>
                                     <form action="{{ route('remove-from-card') }}" method="POST">
+                                        <div class="cart-edit-quantity">
+                                        <input name="quantity" class="num_cart_item form-control" placeholder="0"
+                                            type="number" value="{{ $service->pivot->quantity }}">
+                                            <button class="btn-custom">save</button>
+                                        </div>
                                         @csrf
                                         @method('DELETE')
-                                     <input type="hidden" value="{{ $service->id }}" name="service_id">
-                                     <button type="submit"  class="btn-custom-danger">{{ trans('service.remove') }}</button>
+                                        <input type="hidden" value="{{ $service->id }}" name="service_id">
+                                        <button type="submit"
+                                            class="btn-custom-danger">{{ trans('service.remove') }}</button>
                                     </form>
                                 </div>
                             </div>
